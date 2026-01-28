7480263928c9f8a3086c03416f297995c6ab7799
yanfeng-nie <e8324265@163.com>
2026-01-29 00:53:17 +0800

    adding dependency installing file for QEMU

---
+++ qemu/qemu_dependencies_install.sh
@@ -0,0 +1,11 @@
+apt install re2c
+apt install ninja-build
+apt install build-essential zlib1g-dev pkg-config libglib2.0-dev
+aptinstall binutils-dev libboost-all-dev autoconf libtool libssl-dev libpixman-1-dev libpython-dev python-pip python-capstone virtualenv
+apt install libpixman-1-dev
+apt install bison flex
+apt install meson
+apt install libpixman-1-dev
+apt-get install libpcap-dev libnids-dev libnet1-dev
+apt-get install libattr1-dev
+apt-get install libcap-ng-dev
